9937789eeccc2f4d087ed7dffe752b93919304b5
Jesse <laethaka@gmail.com>
2018-05-27 19:11:36 -0700

    customer JS works

--- bamazon.sql
+++ bamazon.sql
@@ -1,2 +1,31 @@
-create database bamazon
+-- DROP DATABASE IF EXISTS bamazon;
 
+-- create database bamazon;
+
+use bamazon;
+
+-- create table products (
+--     item_id int(11) not null auto_increment,
+--     product_name varchar(30) not null,
+--     department_name varchar(30) not null,
+--     price float(10, 2) not null,
+--     stock_quantity int(11),
+--     primary key (item_id)
+-- );
+
+-- insert into products (product_name, department_name, price, stock_quantity)
+-- values 
+--     ('Infinite Jest', 'fiction', 4.27, 10),
+--     ('Fort Not', 'poetry', 4.30, 10),
+--     ('Jane Eyre', 'fiction', 4.11, 95),
+--     ('End Zone', 'fiction', 3.64, 90),
+--     ('Arcadia', 'play', 4.19, 85),
+--     ('Gorbachev: His Life and Times', 'nonfiction', 4.23, 90),
+--     ('Myra Breckinridge', 'fiction', 3.54, 90),
+--     ('King Lear', 'play', 3.90, 95),
+--     ('Antigone', 'play', 3.81, 90),
+--     ('Cyrano de Bergerac', 'play', 4.05, 95),
+--     ('American Psycho', 'fiction', 3.82, 100),
+--     ('Titus Groan', 'fiction', 3.91, 95);
+
+select * from products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,73 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'bamazon'
+});
+
+var displayTable = new Table({head: ['item_id', 'product_name', 'price'], style: {head:[], border:[], 'padding-left':1, 'padding-right': 1 }})
+
+connection.connect(function(err) {
+    if (err) throw err;
+    connection.query('Select * from products', function(err, res) {
+        if (err) throw err;
+        res.forEach(function(ele) {
+            displayTable.push([ele.item_id, ele.product_name, ele.price]);
+        })
+        console.log(displayTable.toString());
+        userPrompt();
+    })
+})
+
+function userPrompt() {
+    function validateInputId(input) {
+        if (isNaN(input) || input>12 || input<1) {
+            return 'Please pick a valid ID'
+        } else {
+            return true
+        };
+    };
+    function validateInputQuantity(input2) {
+        if (isNaN(input2)) {
+            return 'Please input a number'
+        } else {
+            return true
+        };
+    };
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Welcome to Bamazon! What item ID would you like to buy?',
+            validate: validateInputId,
+            name: 'inputId'
+        },
+        {
+            type: 'input',
+            message: 'How much would you like to buy?',
+            validate: validateInputQuantity,
+            name: 'inputQuantity'
+        }
+    ]).then(function(response) {
+        // console.log(`ok, buying item ${response.inputId}`)
+        updateProduct(response.inputId, response.inputQuantity);
+    });
+};
+
+function updateProduct(id, quantity) {
+    connection.query(`select * from products where item_id = '${id}'`, function(err, res) {
+        if (err) throw err;
+        if (res[0].stock_quantity >= quantity) {
+            console.log(`Valid order confirmed! Your total is $${(quantity * res[0].price).toFixed(2)} for ${quantity} copies of ${res[0].product_name}`)
+            connection.query(`update products set stock_quantity = '${res[0].stock_quantity - quantity}' where item_id = '${id}'`)
+        } else {
+            console.log('Invalid order! Please pick a smaller quantity.')
+        }
+        connection.end();
+    })
+}
